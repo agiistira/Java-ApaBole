a4f7d484dfa9e77b3a0f028b3d77927883f02c0f
agiistira <agiistira@gmail.com>
2022-08-14 02:53:39 +0700

    first commit

---
+++ Apabole.java
@@ -0,0 +1,15 @@
+public class Apabole {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++){
+            if (i%3 ==0 && i%5 ==0){
+                System.out.println("ApaBole");
+            }else if (i%3 ==0){
+                System.out.println("Apa");
+            }else if (i%5 ==0){
+                System.out.println("Bole");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
